33fb9deed09c4e60bbef30770642d766a65108ca
Josh Benner <sjbenner@gmail.com>
2013-11-15 16:42:31 -0800

    Update server.js
    added socket.io

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
+var util = require("util"),
+  io = require("socket.io");
 var http = require('http')
 var port = process.env.PORT || 1337;
+var socket,
+  players;
+  
+
+function init() {
+  players = [];
+  setEventHandlers();
+};
+
+var setEventHandlers = function() {
+  socket.sockets.on("connection", onSocketConnection);
+}
+
+function onSocketConnection(client) {
+  util.log("New player has connected: "+client.id);
+  client.on("disconnect", onClientDisconnect);
+  client.on("new player", onNewPlayer);
+  client.on("move player", onMovePlayer);
+}
+
+function onClientDisconnect() {
+  util.log("Player has disconnected: "+this.id);
+};
+function onNewPlayer(data) {
+  
+};
+function onMovePlayer(data) {
+  
+};
+init();
+
+socket = io.listen(8000);
+
+socket.cofigure(function() {
+  socket.set("transports", ["websocket"]);
+  socket.set("log level", 2);
+})
 http.createServer( function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain'});
   res.end('Hello World\n');
