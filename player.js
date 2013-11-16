282364a518ba977c811c9b2446292d7987f72f09
Josh Benner <sjbenner@gmail.com>
2013-11-15 16:46:59 -0800

    Update player.js
    finished basic object

--- player.js
+++ player.js
@@ -6,4 +6,26 @@ var Player = function(startX, startY) {
     var getX = function() {
       return x;
     };
-}
+
+    var getY = function() {
+      return y;
+    };
+
+    var setX = function(newX) {
+      x = newX;
+    };
+
+    var setY = function(newY) {
+      y = newY;
+    };
+    
+    return {
+      getX: getX,
+      getY: getY,
+      setX: setX,
+      setY: setY,
+      id: id
+    }
+};
+
+exports.Player = Player;
