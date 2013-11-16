7956716451fae50f8970d63891674d9dd355baea
Josh Benner <sjbenner@gmail.com>
2013-11-15 16:52:50 -0800

    Update server.js
    added player connection internals

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var util = require("util"),
   io = require("socket.io");
+Player = require("Player").Player;
 var http = require('http')
 var port = process.env.PORT || 1337;
 var socket,
@@ -26,7 +27,15 @@ function onClientDisconnect() {
   util.log("Player has disconnected: "+this.id);
 };
 function onNewPlayer(data) {
-  
+  var newPlayer = new Player(data.x, data.y);
+  newPlayer.id = this.id;
+  this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()});
+  var i, existingPlayer;
+  for ( i = 0; i < players.length; i++) {
+    existingPlayer = players[i];
+    this.emit("new player", {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()});
+  };
+  players.push(newPlayer);
 };
 function onMovePlayer(data) {
   
