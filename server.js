7477321b48b10685959269ba6fdcb8960616fea5
Josh Benner <sjbenner@gmail.com>
2013-11-20 14:27:39 -0800

    vanilla express app

--- server.js
+++ server.js
@@ -1,54 +1,38 @@
-var util = require("util"),
-  io = require("socket.io");
-Player = require("Player").Player;
-var http = require('http')
-var port = process.env.PORT || 1337;
-var socket,
-  players;
-  
+/**
+ * Module dependencies.
+ */
 
-function init() {
-  players = [];
-  setEventHandlers();
-};
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
-var setEventHandlers = function() {
-  socket.sockets.on("connection", onSocketConnection);
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('nodehackathon2013'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
 
-function onSocketConnection(client) {
-  util.log("New player has connected: "+client.id);
-  client.on("disconnect", onClientDisconnect);
-  client.on("new player", onNewPlayer);
-  client.on("move player", onMovePlayer);
-}
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-function onClientDisconnect() {
-  util.log("Player has disconnected: "+this.id);
-};
-function onNewPlayer(data) {
-  var newPlayer = new Player(data.x, data.y);
-  newPlayer.id = this.id;
-  this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()});
-  var i, existingPlayer;
-  for ( i = 0; i < players.length; i++) {
-    existingPlayer = players[i];
-    this.emit("new player", {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()});
-  };
-  players.push(newPlayer);
-};
-function onMovePlayer(data) {
-  
-};
-init();
-
-socket = io.listen(8000);
-
-socket.cofigure(function() {
-  socket.set("transports", ["websocket"]);
-  socket.set("log level", 2);
-})
-http.createServer( function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
