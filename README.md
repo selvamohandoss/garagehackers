339f37ebd621bd0047665324d30db747075686f8
Josh Benner <sjbenner@gmail.com>
2013-11-21 15:03:22 -0800

    brute force merge

--- README.md
+++
@@ -1,4 +0,0 @@
-garagehackers
-=============
-
-Initial repo for node.js hackathon at "The Garage" http://msopentech.com/nodehackathon2013/.
--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,5 @@
  */
 
 exports.index = function(req, res){
-    var access_token = req.query.access_token;
-    var options = {
-        host: 'api.cbssports.com',
-        port: 80,
-        path: 'fantasy/league/teams?access_token=' + access_token + '&response_format=json'
-    };
-
-    http.get(options, function(res) {
-        var data = '';
-        
-        res.on('data', function  (chunk) {
-            data += chunk;
-        });
-        res.on('end', function  () {
-            var obj = JSON.parse(data);
-            res.render('index', { title: 'The game', teams: JSON.stringify(obj.body.teams) });
-        });
-    }).on('error', function(e) {
-        res.render('index', { title: 'The game', teams: e.message });
-    });
+  res.render('index', { title: 'Node Planes' });
 };
