4dabf8890d7b2434d97829793a7ad564b042ff65
Josh Benner <sjbenner@gmail.com>
2013-11-20 16:13:46 -0800

    request team data

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,28 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'The game' });
+    var access_token = req.query.access_token;
+    var options = {
+        host: 'api.cbssports.com',
+        port: 80,
+        path: 'fantasy/league/teams?access_token=' + access_token + '&response_format=json'
+    };
+
+    cbs_request(options);
+};
+
+
+function cbs_request(options) {
+
+    http.get(options, function(res) {
+        var data = '';
+        
+        res.on('data', function  (chunk) {
+            data += chunk;
+        });
+        res.on('end', function  () {
+            var obj = JSON.parse(data);
+            res.render('index', { title: 'The game', teams: JSON.stringify(obj.body.teams) });
+        });
+    });
 };
