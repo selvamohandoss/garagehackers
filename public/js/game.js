6a2e7b18dcdf6831234a6f179dcd2aad11d3e2aa
Josh Benner <sjbenner@gmail.com>
2013-11-25 10:45:53 -0800

    split game state and animation loops

--- public/js/game.js
+++ public/js/game.js
@@ -6,7 +6,12 @@ function InitializeCanvas() {
     var ctx = document.getElementById('game-view').getContext('2d');
     var p = new window.BIPLANES.Airplane({ x: 50, y: 50, rotationAngle: 0, ctx: ctx});
     var lastTimeStamp = (new Date()).valueOf();
-    function render(){
+    function gameLoop(){
+        p.update( (new Date()).valueOf() - lastTimeStamp );
+        lastTimeStamp = (new Date()).valueOf();
+    }
+
+    function renderLoop() {
         ctx.clearRect(0,0,640,480);
         ctx.rect(0,0, 640, 480);
         var gradient = ctx.createLinearGradient(0,0,0,480);
@@ -16,10 +21,9 @@ function InitializeCanvas() {
         gradient.addColorStop(1, '#007700');
         ctx.fillStyle = gradient;
         ctx.fill();
-        p.update( (new Date()).valueOf() - lastTimeStamp );
-        lastTimeStamp = (new Date()).valueOf();
-
         p.Draw();
+        requestAnimationFrame( renderLoop );
     }
-    setInterval(render, 5);
+    setInterval(gameLoop, 5);
+    requestAnimationFrame( renderLoop );
 };
